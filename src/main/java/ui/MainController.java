cc77318fada5f064789b4b8a2c9bcb6589176ff4
chrstecher <chrstecher@tsn.at>
2023-06-18 01:27:58 +0200

    Video R - Main FXML Teil 2 + Programmierung fertiggestellt

--- src/main/java/ui/MainController.java
+++ src/main/java/ui/MainController.java
@@ -10,24 +10,28 @@ import javafx.scene.paint.Color;
 
 public class MainController {
 
-    private final String javafxVersion = System.getProperty("javafx.version");
-
-    @FXML
-    private Label color;
-
-    @FXML
-    private Label label;
-
-    @FXML
-    private ResourceBundle resources;
 
     public void initialize() {
-        label.setText(String.format(resources.getString("label.text"), "JavaFX", javafxVersion));
+
     }
 
-    @FXML
-    private void updateColor() {
-        Random random = new Random();
-        this.color.setTextFill(Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256)));
+    public void deposit()
+    {
+        System.out.println("DEPOSIT");
+    }
+
+    public void withdraw()
+    {
+        System.out.println("WITHDRAW");
+    }
+
+    public void openWallet()
+    {
+        System.out.println("OPEN WALLET");
+    }
+
+    public void newWallet()
+    {
+        System.out.println("NEW WALLET");
     }
 }
