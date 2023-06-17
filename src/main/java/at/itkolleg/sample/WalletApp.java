051c3ef6af183a9e77be9b55f00fff4449a38422
chrstecher <chrstecher@tsn.at>
2023-06-18 00:40:32 +0200

    Video P - WalletApp Klasse Teil 3 - Konsolidieren + Programmierung fertiggestellt

--- src/main/java/at/itkolleg/sample/WalletApp.java
+++ src/main/java/at/itkolleg/sample/WalletApp.java
@@ -113,7 +113,7 @@ public class WalletApp extends Application {
     }
 
     @Override
-    public void stop()
+    public void stop() //Geänderte Daten von WalletList und BankAccount werden in eine Datei hinausgespeichert, nach Beendigung der Applikation
     {
         WalletList walletList = (WalletList)GlobalContext.getGlobalContext().getStateFor(WalletApp.GLOBAL_WALLET_LIST);
         BankAccount bankAccount = (BankAccount) GlobalContext.getGlobalContext().getStateFor(WalletApp.GLOBAL_BANK_ACCOUNT);
@@ -136,94 +136,6 @@ public class WalletApp extends Application {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hallo Welt!");
-        BankAccount ba = new BankAccount();
-        ba.deposit(new BigDecimal("1000"));
-        System.out.println(ba);
-
-        CryptoCurrency crypto = CryptoCurrency.BTC;
-        System.out.println(crypto.getCurrencyName());
-        System.out.println(crypto.getCode());
-        System.out.println(CryptoCurrency.valueOf("BTC").getCurrencyName());
-
-        Transaction transaction = new Transaction(CryptoCurrency.ETH,
-                new BigDecimal("1.23"),
-                new BigDecimal("1567.8")
-        );
-
-        System.out.println(transaction);
-
-        Wallet wallet = null;
-        try {
-            wallet = new Wallet("My BTC Wallet", CryptoCurrency.BTC, new BigDecimal("1"));
-        } catch (InvalidFeeException e) {
-            System.out.println(e.getMessage()); //Ausgabe der Nachricht der Exception
-            e.printStackTrace(); //Ausgabe wie diese Exception zustande kam
-        }
-        System.out.println(wallet);
-
-        try {
-            wallet.buy(new BigDecimal("10"), new BigDecimal("5"), ba);
-        } catch (InvalidAmountException e) {
-            e.printStackTrace();
-        } catch (InsufficientBalanceException insufficientBalanceException) {
-            insufficientBalanceException.printStackTrace();
-        }
-
-        System.out.println(ba);
-        System.out.println(wallet);
-        try {
-            wallet.sell(new BigDecimal("10"), new BigDecimal("5"), ba);
-        } catch (InsufficientAmountException e) {
-            e.printStackTrace();
-        } catch (InvalidAmountException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println(ba);
-        System.out.println(wallet);
-
-        WalletList walletList = new WalletList();
-        walletList.addWallet(wallet);
-
-        System.out.println(walletList);
-
-        CurrentPriceForCurrency currentPrices = new CurrentCurrencyPrices();
-        try {
-            BigDecimal result = currentPrices.getCurrentPrice(CryptoCurrency.BTC);
-            System.out.println(result);
-        } catch (GetCurrentPriceException e) {
-            e.printStackTrace();
-        }
-
-        DataStore dataStore = new FileDataStore();
-        try {
-            dataStore.saveBankAccount(ba);
-        } catch (SaveDataException e) {
-            //e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            BankAccount bankAccount2 = dataStore.retrieveBankAccount();
-            System.out.println(bankAccount2);
-        } catch (RetrieveDataException e) {
-            //e.printStackTrace();
-            System.out.println(e.getMessage());
-        }
-
-        try {
-            dataStore.saveWalletList(walletList);
-        } catch (SaveDataException e) {
-            e.printStackTrace();
-        }
-        try {
-            WalletList walletList2 = dataStore.retrieveWalletList();
-            System.out.println(walletList2);
-        } catch (RetrieveDataException e) {
-            e.printStackTrace();
-        }
-
         launch(args);
     }
 }
