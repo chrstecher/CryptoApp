9d7a8ce1f3a01f2fc3b1a47dc3b077694a6f286a
chrstecher <chrstecher@tsn.at>
2023-06-20 16:01:48 +0200

    Video Y - WalletController Teil 3 + Programmierung fertiggestellt

--- src/main/java/ui/WalletController.java
+++ src/main/java/ui/WalletController.java
@@ -9,7 +9,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -30,12 +32,42 @@ public class WalletController {
     {
         this.wallet = (Wallet) GlobalContext.getGlobalContext().getStateFor(WalletApp.GLOBAL_SELECTED_WALLET);
 
+        populateTable();
         refreshAllGuiValues();
 
         btnBackToMain.setOnAction((ActionEvent e) ->
         {
             WalletApp.switchScene("main.fxml", "at.itkolleg.sample.main");
         });
+    }
+
+    private void populateTable()
+    {
+        TableColumn<Transaction, String> id = new TableColumn<>("ID");
+        id.setCellValueFactory(new PropertyValueFactory<>("id"));
+
+        TableColumn<Transaction, String> crypto = new TableColumn<>("CRYPTO");
+        crypto.setCellValueFactory(new PropertyValueFactory<>("cryptoCurrency"));
+
+        TableColumn<Transaction, String> amount = new TableColumn<>("AMOUNT");
+        amount.setCellValueFactory(new PropertyValueFactory<>("amount"));
+
+        TableColumn<Transaction, String> total = new TableColumn<>("TOTAL");
+        total.setCellValueFactory(new PropertyValueFactory<>("total"));
+
+        TableColumn<Transaction, String> priceAtTransactionDate = new TableColumn<>("PRICE");
+        priceAtTransactionDate.setCellValueFactory(new PropertyValueFactory<>("priceAtTransactionDate"));
+
+        TableColumn<Transaction, String> date = new TableColumn<>("DATE");
+        date.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        tblTransactions.getColumns().clear();
+        tblTransactions.getColumns().add(id);
+        tblTransactions.getColumns().add(crypto);
+        tblTransactions.getColumns().add(amount);
+        tblTransactions.getColumns().add(total);
+        tblTransactions.getColumns().add(priceAtTransactionDate);
+        tblTransactions.getColumns().add(date);
     }
     public void buy()
     {
@@ -47,7 +79,7 @@ public class WalletController {
         return (CurrentPriceForCurrency) GlobalContext.getGlobalContext().getStateFor(WalletApp.GLOBAL_CURRENT_CURRENCY_PRICES);
     }
 
-    public void refreshAllGuiValues()
+    private void refreshAllGuiValues()
     {
         this.lblId.textProperty().setValue(this.wallet.getId().toString());
         this.lblName.textProperty().setValue(this.wallet.getName());
@@ -65,6 +97,8 @@ public class WalletController {
             this.lblValue.textProperty().setValue("CURRENT PRICES UNAVAILABLE!");
             e.printStackTrace();
         }
+
+        tblTransactions.getItems().setAll(wallet.getTransactions());
     }
 
     public void sell()
